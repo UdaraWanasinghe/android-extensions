cf46659d9d929133ad8984c418f462660a5fe032
Udara Wanasinghe <udara.developer@gmail.com>
2022-10-05 21:32:27 +0530

    [refactor] remove unwanted resources

--- extensions/src/androidTest/java/com/aureusapps/android/extensions/ExtensionsInstrumentedTests.kt
+++ extensions/src/androidTest/java/com/aureusapps/android/extensions/ExtensionsInstrumentedTests.kt
@@ -6,7 +6,6 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     UrlExtensionsInstrumentedTest::class,
-    ContextExtensionsInstrumentedTest::class,
     RoomDatabaseExtensionsInstrumentedTest::class,
     InputStreamExtensionsInstrumentedTest::class
 )
