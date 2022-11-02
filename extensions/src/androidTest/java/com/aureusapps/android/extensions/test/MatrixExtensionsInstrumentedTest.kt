abc9a173131c5b63e1fbbe95d7b40fcb8432e2dc
Udara Wanasinghe <udara.developer@gmail.com>
2022-11-02 13:52:29 +0530

    update matrix extensions and instrumented tests

--- extensions/src/androidTest/java/com/aureusapps/android/extensions/test/MatrixExtensionsInstrumentedTest.kt
+++ extensions/src/androidTest/java/com/aureusapps/android/extensions/test/MatrixExtensionsInstrumentedTest.kt
@@ -3,7 +3,9 @@ package com.aureusapps.android.extensions.test
 import android.graphics.Matrix
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.aureusapps.android.extensions.getRotation
+import com.aureusapps.android.extensions.getScaling
 import com.aureusapps.android.extensions.rotation
+import com.aureusapps.android.extensions.scaling
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,7 +14,7 @@ import org.junit.runner.RunWith
 class MatrixExtensionsInstrumentedTest {
 
     @Test
-    fun testMatrixRotationAroundPivot() {
+    fun testMatrixGetRotationAroundPivot() {
         // only rotate
         val m = Matrix()
         m.postRotate(45f)
@@ -24,14 +26,45 @@ class MatrixExtensionsInstrumentedTest {
         // rotate around pivot and scale
         m.reset()
         m.postRotate(45f, 10f, 10f)
-        m.postScale(2f, 2f)
+        m.postScale(2f, 2f, 10f, 10f)
         Assert.assertEquals(45f, m.getRotation(10f, 10f))
         // rotate around pivot, scale and translate
         m.reset()
         m.postRotate(45f, 10f, 10f)
-        m.postScale(2f, 2f)
+        m.postScale(2f, 2f, 10f, 10f)
         m.postTranslate(10f, 10f)
         Assert.assertEquals(45f, m.getRotation(20f, 20f))
+    }
+
+    @Test
+    fun testMatrixGetScalingAroundPivot() {
+        // only scale
+        val m = Matrix()
+        m.postScale(2f, 2f)
+        var s = m.scaling
+        Assert.assertEquals(2f, s.first, 0.00001f)
+        Assert.assertEquals(2f, s.second, 0.00001f)
+        // scale around pivot
+        m.reset()
+        m.postScale(2f, 2f, 10f, 10f)
+        s = m.getScaling(10f, 10f)
+        Assert.assertEquals(2f, s.first, 0.00001f)
+        Assert.assertEquals(2f, s.second, 0.00001f)
+        // scale around pivot and rotate
+        m.reset()
+        m.postScale(2f, 2f, 10f, 10f)
+        m.postRotate(45f, 10f, 10f)
+        s = m.getScaling(10f, 10f)
+        Assert.assertEquals(2f, s.first, 0.00001f)
+        Assert.assertEquals(2f, s.second, 0.00001f)
+        // scale around pivot, rotate and translate
+        m.reset()
+        m.postScale(2f, 2f, 10f, 10f)
+        m.postRotate(45f, 10f, 10f)
+        m.postTranslate(10f, 10f)
+        s = m.getScaling(20f, 20f)
+        Assert.assertEquals(2f, s.first, 0.00001f)
+        Assert.assertEquals(2f, s.second, 0.00001f)
     }
 
 }
