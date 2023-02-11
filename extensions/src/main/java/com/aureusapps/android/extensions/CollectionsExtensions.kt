9c935d1edbc643de3985e8ea2715f347be04bd23
Udara Wanasinghe <udara.developer@gmail.com>
2023-02-11 12:42:50 +0530

    added collections extensions

--- extensions/src/main/java/com/aureusapps/android/extensions/CollectionsExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/CollectionsExtensions.kt
@@ -3,3 +3,7 @@ package com.aureusapps.android.extensions
 fun <T> Collection<T>.copyOf(): Collection<T> {
     return mutableListOf<T>().also { it.addAll(this) }
 }
+
+fun <T> MutableList<T>.copyOf(): MutableList<T> {
+    return mutableListOf<T>().also { it.addAll(this) }
+}
