d59e01f235d8bea111c669582e5b60b6c694f5aa
Udara Wanasinghe <udara.developer@gmail.com>
2023-02-17 16:53:39 +0530

    use yield function in input stream extension to increase responsiveness

--- extensions/src/main/java/com/aureusapps/android/extensions/InputStreamExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/InputStreamExtensions.kt
@@ -2,53 +2,65 @@ package com.aureusapps.android.extensions
 
 import android.content.Context
 import android.net.Uri
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import java.io.*
 
-fun InputStream.writeTo(path: String, bufferSize: Int = 8192): Long {
-    val file = File(path)
-    val output = BufferedOutputStream(FileOutputStream(file))
-    val input = BufferedInputStream(this)
-    val buffer = ByteArray(bufferSize)
-    var read: Int
-    var size = 0L
-    while (input.read(buffer).also { read = it } != -1) {
-        size += read
-        output.write(buffer, 0, read)
+suspend fun InputStream.writeTo(path: String, bufferSize: Int = 8192): Long {
+    return withContext(Dispatchers.IO) {
+        val file = File(path)
+        val output = BufferedOutputStream(FileOutputStream(file))
+        val input = BufferedInputStream(this@writeTo)
+        val buffer = ByteArray(bufferSize)
+        var read: Int
+        var size = 0L
+        while (input.read(buffer).also { read = it } != -1) {
+            size += read
+            output.write(buffer, 0, read)
+            yield()
+        }
+        output.flush()
+        output.close()
+        input.close()
+        size
     }
-    output.flush()
-    output.close()
-    input.close()
-    return size
 }
 
-fun InputStream.writeTo(uri: Uri, context: Context, bufferSize: Int = 8192): Long {
-    val output = BufferedOutputStream(context.contentResolver.openOutputStream(uri))
-    val input = BufferedInputStream(this)
-    val buffer = ByteArray(bufferSize)
-    var read: Int
-    var size = 0L
-    while (input.read(buffer).also { read = it } != -1) {
-        size += read
-        output.write(buffer, 0, read)
+suspend fun InputStream.writeTo(uri: Uri, context: Context, bufferSize: Int = 8192): Long {
+    return withContext(Dispatchers.IO) {
+        val output = BufferedOutputStream(context.contentResolver.openOutputStream(uri))
+        val input = BufferedInputStream(this@writeTo)
+        val buffer = ByteArray(bufferSize)
+        var read: Int
+        var size = 0L
+        while (input.read(buffer).also { read = it } != -1) {
+            size += read
+            output.write(buffer, 0, read)
+            yield()
+        }
+        output.flush()
+        output.close()
+        input.close()
+        size
     }
-    output.flush()
-    output.close()
-    input.close()
-    return size
 }
 
-fun InputStream.writeTo(out: OutputStream, bufferSize: Int = 8192): Long {
-    val input = BufferedInputStream(this)
-    val output = BufferedOutputStream(out)
-    val buffer = ByteArray(bufferSize)
-    var read: Int
-    var size = 0L
-    while (input.read(buffer).also { read = it } != -1) {
-        size += read
-        output.write(buffer, 0, read)
+suspend fun InputStream.writeTo(out: OutputStream, bufferSize: Int = 8192): Long {
+    return withContext(Dispatchers.IO) {
+        val input = BufferedInputStream(this@writeTo)
+        val output = BufferedOutputStream(out)
+        val buffer = ByteArray(bufferSize)
+        var read: Int
+        var size = 0L
+        while (input.read(buffer).also { read = it } != -1) {
+            size += read
+            output.write(buffer, 0, read)
+            yield()
+        }
+        output.flush()
+        output.close()
+        input.close()
+        size
     }
-    output.flush()
-    output.close()
-    input.close()
-    return size
 }
