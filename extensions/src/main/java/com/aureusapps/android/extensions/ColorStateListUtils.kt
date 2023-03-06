9c3e039d035cca98d7abc33e01b136b5df547bdb
Udara Wanasinghe <udara.developer@gmail.com>
2023-03-06 13:55:17 +0530

    added color state list utils

---
+++ extensions/src/main/java/com/aureusapps/android/extensions/ColorStateListUtils.kt
@@ -0,0 +1,8 @@
+package com.aureusapps.android.extensions
+
+import android.content.res.ColorStateList
+
+fun colorStateListOf(vararg mapping: Pair<IntArray, Int>): ColorStateList {
+    val (states, colors) = mapping.unzip()
+    return ColorStateList(states.toTypedArray(), colors.toIntArray())
+}
