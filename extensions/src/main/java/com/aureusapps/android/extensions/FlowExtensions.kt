07284896a3e29d3c7c264f5c11fc598f0e23399a
Udara Wanasinghe <udara.developer@gmail.com>
2023-06-20 04:26:14 +0530

    add flow extension function: scanNonNull

---
+++ extensions/src/main/java/com/aureusapps/android/extensions/FlowExtensions.kt
@@ -0,0 +1,29 @@
+package com.aureusapps.android.extensions
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/**
+ * Folds the given flow with operation, emitting every intermediate result that is not null, including initial value.
+ *
+ * @param initial Initial value to emit.
+ * @param operation Accumulation operation.
+ *
+ * @return Returns new flow with the applied operation to accumulate non-null values.
+ */
+fun <T, R> Flow<T>.scanNotNull(
+    initial: R,
+    operation: suspend (accumulator: R, value: T) -> R?
+): Flow<R> {
+    return flow {
+        var accumulator: R = initial
+        emit(initial)
+        collect { value ->
+            val newValue = operation(accumulator, value)
+            if (newValue != null) {
+                accumulator = newValue
+                emit(newValue)
+            }
+        }
+    }
+}
