789c54446e9a1e98650c5f60c3aa01829dafb46c
Udara Wanasinghe <udara.developer@gmail.com>
2023-08-26 17:34:09 +0530

    add matrix operator function: times

--- extensions/src/main/java/com/aureusapps/android/extensions/MatrixExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/MatrixExtensions.kt
@@ -48,3 +48,12 @@ fun Matrix.copy(matrix: Matrix = Matrix()): Matrix {
     matrix.set(this)
     return matrix
 }
+
+/**
+ * Multiplies this matrix by the given matrix. M' = M * other
+ */
+operator fun Matrix.times(matrix: Matrix): Matrix {
+    val m = Matrix(this)
+    m.preConcat(matrix)
+    return m
+}
