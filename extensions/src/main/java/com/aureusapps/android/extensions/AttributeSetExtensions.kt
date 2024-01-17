c26b13b82b448240385afbfce4068aeabfe1f96f
Udara Wanasinghe <udara.developer@gmail.com>
2024-01-17 10:51:57 +0530

    add extension func: get color state list from attribute set

--- extensions/src/main/java/com/aureusapps/android/extensions/AttributeSetExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/AttributeSetExtensions.kt
@@ -1,10 +1,13 @@
 package com.aureusapps.android.extensions
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.util.AttributeSet
 import android.util.TypedValue
+import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 
 private val dimenMap = mapOf(
     "px" to TypedValue.COMPLEX_UNIT_PX,
@@ -16,6 +19,16 @@ private val dimenMap = mapOf(
     "mm" to TypedValue.COMPLEX_UNIT_MM
 )
 
+/**
+ * Retrieves the color attribute value specified by the attribute name from the AttributeSet.
+ *
+ * @param context The context used to obtain resources and display metrics.
+ * @param attrName The name of the color attribute to retrieve from the AttributeSet.
+ * @param defaultColor The default color to be returned if the attribute is not present or
+ *        cannot be resolved. Default is Color.BLACK.
+ *
+ * @return The color integer if the attribute is present, or the default color if not found.
+ */
 fun AttributeSet.getColorAttribute(
     context: Context,
     attrName: String,
@@ -32,6 +45,7 @@ fun AttributeSet.getColorAttribute(
                     val colorResId = attrValue.replace("@", "").toInt()
                     ContextCompat.getColor(context, colorResId)
                 }
+
                 attrValue.contains("?") -> {
                     val colorAttrId = attrValue.replace("?", "").toInt()
                     val outValue = TypedValue()
@@ -41,6 +55,7 @@ fun AttributeSet.getColorAttribute(
                         defaultColor
                     }
                 }
+
                 else -> {
                     getAttributeUnsignedIntValue(attrIndex, defaultColor)
                 }
@@ -51,6 +66,58 @@ fun AttributeSet.getColorAttribute(
     }
 }
 
+/**
+ * Retrieves the ColorStateList attribute specified by the attribute name from the AttributeSet.
+ *
+ * @param context The context used to obtain resources and display metrics.
+ * @param attrName The name of the ColorStateList attribute to retrieve from the AttributeSet.
+ *
+ * @return The ColorStateList if the attribute is present, or null if not found.
+ */
+fun AttributeSet.getColorStateListAttribute(
+    context: Context,
+    attrName: String,
+): ColorStateList? {
+    val attrIndex = getAttributePosition(attrName)
+    return if (attrIndex < 0) {
+        null
+    } else {
+        val attrValue = getAttributeValue(attrIndex)
+        return when {
+            attrValue.startsWith("@") -> {
+                val colorResId = attrValue.replace("@", "").toInt()
+                getColorStateList(context, colorResId)
+            }
+
+            attrValue.startsWith("?") -> {
+                val colorAttrId = attrValue.replace("?", "").toInt()
+                val outValue = TypedValue()
+                if (context.theme.resolveAttribute(colorAttrId, outValue, true)) {
+                    getColorStateList(context, outValue.resourceId)
+                } else {
+                    null
+                }
+            }
+
+            else -> ColorStateList.valueOf(
+                getAttributeUnsignedIntValue(attrIndex, Color.TRANSPARENT)
+            )
+        }
+    }
+}
+
+private fun getColorStateList(context: Context, @ColorRes colorResId: Int): ColorStateList? {
+    return ResourcesCompat.getColorStateList(context.resources, colorResId, context.theme)
+}
+
+/**
+ * Retrieves the float attribute value specified by the attribute name from the AttributeSet.
+ *
+ * @param context The context used to obtain resources and display metrics.
+ * @param attrName The name of the float attribute to retrieve from the AttributeSet.
+ *
+ * @return The float value if the attribute is present, or null if not found.
+ */
 fun AttributeSet.getFloatAttribute(context: Context, attrName: String): Float? {
     val attrIndex = getAttributePosition(attrName)
     return if (attrIndex < 0) {
@@ -62,6 +129,7 @@ fun AttributeSet.getFloatAttribute(context: Context, attrName: String): Float? {
                 val floatResId = attrValue.replace("@", "").toInt()
                 context.resources.getDimension(floatResId)
             }
+
             attrValue.startsWith("?") -> {
                 val floatAttrId = attrValue.replace("?", "").toInt()
                 val outValue = TypedValue()
@@ -71,6 +139,7 @@ fun AttributeSet.getFloatAttribute(context: Context, attrName: String): Float? {
                     null
                 }
             }
+
             else -> {
                 getAttributeFloatValue(attrIndex, 0f)
             }
@@ -78,6 +147,14 @@ fun AttributeSet.getFloatAttribute(context: Context, attrName: String): Float? {
     }
 }
 
+/**
+ * Retrieves the dimension attribute value specified by the attribute name from the AttributeSet.
+ *
+ * @param context The context used to obtain the display metrics for unit conversion.
+ * @param attrName The name of the dimension attribute to retrieve from the AttributeSet.
+ *
+ * @return The dimension value in pixels if the attribute is present, or null if not found.
+ */
 fun AttributeSet.getDimensionAttribute(context: Context, attrName: String): Float? {
     val attrIndex = getAttributePosition(attrName)
     if (attrIndex < 0) {
@@ -89,6 +166,7 @@ fun AttributeSet.getDimensionAttribute(context: Context, attrName: String): Floa
             val dimenResId = attrValue.replace("@", "").toInt()
             context.resources.getDimension(dimenResId)
         }
+
         attrValue.startsWith("?") -> {
             val dimenAttrId = attrValue.replace("?", "").toInt()
             val outValue = TypedValue()
@@ -98,6 +176,7 @@ fun AttributeSet.getDimensionAttribute(context: Context, attrName: String): Floa
                 null
             }
         }
+
         else -> {
             val dimenUnit = dimenMap.entries.firstOrNull { attrValue.endsWith(it.key) }
             if (dimenUnit == null) {
@@ -113,7 +192,18 @@ fun AttributeSet.getDimensionAttribute(context: Context, attrName: String): Floa
     }
 }
 
+/**
+ * Retrieves the position of the specified attribute in the AttributeSet.
+ *
+ * This function iterates through the attributes in the AttributeSet and returns
+ * the position (index) of the attribute with the given name. If the attribute is
+ * not found, it returns -1.
+ *
+ * @param attrName The name of the attribute whose position is to be retrieved.
+ * @return The position (index) of the attribute in the AttributeSet, or -1 if not found.
+ * @throws IllegalArgumentException if the provided attribute name is empty.
+ */
 fun AttributeSet.getAttributePosition(attrName: String): Int {
-    return (0 until attributeCount)
-        .firstOrNull { i -> getAttributeName(i) == attrName } ?: -1
+    require(attrName.isNotBlank()) { "Attribute name must not be empty." }
+    return (0 until attributeCount).firstOrNull { i -> getAttributeName(i) == attrName } ?: -1
 }
