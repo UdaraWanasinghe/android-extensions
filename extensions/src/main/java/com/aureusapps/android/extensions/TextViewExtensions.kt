e41c13ab45f635bd73971705e3a836a8ac8aa51f
Udara Wanasinghe <udara.developer@gmail.com>
2024-02-09 04:19:49 +0530

    make text view extensions public

--- extensions/src/main/java/com/aureusapps/android/extensions/TextViewExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/TextViewExtensions.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
  *
  * @param text The new text to set in the TextView.
  */
-private fun TextView.updateText(text: String) {
+fun TextView.updateText(text: String) {
     setTag(R.id.text_view_user_edit_key, false)
     setText(text)
     setTag(R.id.text_view_user_edit_key, true)
@@ -18,7 +18,7 @@ private fun TextView.updateText(text: String) {
  *
  * @return True if the text has been edited by the user, false otherwise.
  */
-private val TextView.isUserEdit: Boolean
+val TextView.isUserEdit: Boolean
     get() {
         val tag = getTag(R.id.text_view_user_edit_key)
         return tag == null || tag == true
