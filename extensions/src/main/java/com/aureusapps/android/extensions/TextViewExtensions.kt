af9fd9756f95332c6aebe6136901fc1f7ab4918c
Udara Wanasinghe <udara.developer@gmail.com>
2024-02-09 04:12:01 +0530

    add text view extensions to determine whether text is edited by use or not

--- extensions/src/main/java/com/aureusapps/android/extensions/TextViewExtensions.kt
+++ extensions/src/main/java/com/aureusapps/android/extensions/TextViewExtensions.kt
@@ -12,3 +12,25 @@ fun TextView.setTextStyle(@StyleRes resId: Int) {
         setTextAppearance(context, resId)
     }
 }
+
+/**
+ * Updates the text of the TextView and sets a tag to indicate whether the text was edited by the user or not.
+ *
+ * @param text The new text to set in the TextView.
+ */
+private fun TextView.updateText(text: String) {
+    setTag(R.id.text_view_user_edit_key, false)
+    setText(text)
+    setTag(R.id.text_view_user_edit_key, true)
+}
+
+/**
+ * Determines whether the text of the TextView has been edited by the user.
+ *
+ * @return True if the text has been edited by the user, false otherwise.
+ */
+private val TextView.isUserEdit: Boolean
+    get() {
+        val tag = getTag(R.id.text_view_user_edit_key)
+        return tag == null || tag == true
+    }
